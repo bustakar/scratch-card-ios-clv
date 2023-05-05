db959486b96898d81a6e2327e748777c1e727f5d
Karel Bušta <karel.busta@cleverlance.com>
2023-05-05 06:30:31 +0200

    Initial Commit

---
+++ ScratchCard/ScratchCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchCardApp.swift
+//  ScratchCard
+//
+//  Created by Karel Bušta on 05.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScratchCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
